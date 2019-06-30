c4adf2f709ee3a239ff28d8ebdb3b47a4877b4b8
glaubermarcelino <gmsoft.informatica@gmail.com>
2019-06-30 06:58:10 -0300

    Configuração de HotReload 1.4.x Xamarin Forms

---
+++ ConfigHotReload/ConfigHotReload/ConfigHotReload/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ConfigHotReload
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+#if DEBUG
+            HotReloader.Current.Run(this, new HotReloader.Configuration
+            {
+                //Porta padrão para seu dispositivo
+                DeviceUrlPort = 8002
+            });
+#endif
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
